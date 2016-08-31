e7d3a57e911d1b45c2e1957cfd34f9e19b386a7c
Imam & Sahbana <pair+imamnugraha23.sahbanagold@hacktiv8.com>
2016-09-01 01:27:11 +0700

    sahbana & imam

--- method_chaining.js
+++ method_chaining.js
@@ -1,9 +1,9 @@
 // TODO: Refactor for elegance
 function shout_backwards(data){
-  var all_caps = data.toUpperCase();
-  var splitting = all_caps.split("");
-  var reversing = splitting.reverse()
-  var backwards = reversing.join("");
+  var backwards = data.toUpperCase()
+                      .split("")
+                      .reverse()
+                      .join("");
   return backwards + "!!!";
 }
 
--- method_chaining_from_scratch.js
+++ method_chaining_from_scratch.js
@@ -1,21 +1,47 @@
- // The data store:
- var usersData = [
-   {firstName:"SpongeBob", lastName:"SquarePants", email:"spongebob@crustycrab.com", id:102},
-   {firstName:"Patrick", lastName:"Star", email:"patric.star@gmail.com", id:103},
-   {firstName:"Squidward", lastName:"Tentacles", email:"awesomesquidward@yahoo.com", id:104}
- ]
+// The data store:
+var usersData = [
+  {firstName:"SpongeBob", lastName:"SquarePants", email:"spongebob@crustycrab.com", id:102},
+  {firstName:"Patrick", lastName:"Star", email:"patric.star@gmail.com", id:103},
+  {firstName:"Squidward", lastName:"Tentacles", email:"awesomesquidward@yahoo.com", id:104}
+]
 
- function titleCaseName(str)
- {
+function titleCaseName(str)
+{
 
- }
-   // Our object with the chainable methods
- var userController = {
-
- };
+}
+// Our object with the chainable methods
+var userController = {
+  user : {},
+  formatedData : "",
+  formatedName : "",
+  findUser(email){
+    for (var i = 0; i < usersData.length; i++) {
+      if(usersData[i]["email"] = email){
+        this.user = usersData[i]
+        break
+      }
+    }
+    return this
+  },
+  formatName(){
+    this.formatedName = "Member name: "+this.user["firstName"]+" "+this.user["lastName"]
+    return this
+  },
+  formatData(){
+    this.formatedData = "ID: "+this.user["id"]+"\n"+"Email:"+this.user["email"]
+    return this
+  },
+  displayUser(){
+    console.log(this.formatedName);
+    console.log(this.formatedData);
+  }
+};
 
 // Driver code
-userController.findUser("awesomesquidward@yahoo.com").formatName().formatData().displayUser();
+userController.findUser("awesomesquidward@yahoo.com")
+.formatName()
+.formatData()
+.displayUser();
 
 // result
 // Member name: Squidward Tentacles
