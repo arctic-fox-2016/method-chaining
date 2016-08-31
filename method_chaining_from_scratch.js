28ece1a87f8ba4cb04f9918f92c63ae0f0d91c66
Ryan Andhaka & Toni <pair+ryanandhaka.tonychen09@hacktiv8.com>
2016-08-31 19:41:04 +0700

    methodchaining

--- method_chaining_from_scratch.js
+++ method_chaining_from_scratch.js
@@ -7,10 +7,41 @@
 
  function titleCaseName(str)
  {
-
+   return str.replace(/\w\S*/g, function(txt)
+   {return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+   });
  }
-   // Our object with the chainable methods
  var userController = {
+   currentUser:"",
+   findUser:function (userEmail) {
+    var arrayLength = usersData.length, i;
+    for (i = arrayLength - 1; i >= 0; i--) {
+     if (usersData[i].email === userEmail) {
+      this.currentUser = usersData[i];
+      break;
+     }
+    }
+
+    return this;
+   },
+   formatName:function () {
+    if (this.currentUser) {
+     this.currentUser.fullName = titleCaseName (this.currentUser.firstName) + " " + titleCaseName (this.currentUser.lastName);
+    }
+    return this;
+
+   },
+
+   formatData:function () {
+    if (this.currentUser) {
+     this.currentUser.viewData = "<h2>Member: " + this.currentUser.fullName + "</h2>"  + "<p>ID: " + this.currentUser.id + "</p>" + "<p>Email: " + this.currentUser.email + "</p>";
+    }
+    return this;
+   },
+
+   displayUser:function () {
+    if (!this.currentUser) return;
+   }
 
  };
 
