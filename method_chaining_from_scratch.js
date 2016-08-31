a3433702965604710c619b0047dc6ac0a9ebe6e3
Ari Adiprana & Tevin <pair+ariadiprana.tevinstein@hacktiv8.com>
2016-08-31 18:13:12 +0700

    add files

--- method_chaining_from_scratch.js
+++ method_chaining_from_scratch.js
@@ -6,16 +6,51 @@
  ]
 
  function titleCaseName(str)
- {
+  {
+   return str;
+  }
+  // Our object with the chainable methods
+  var userController = {
 
- }
-   // Our object with the chainable methods
- var userController = {
+   currentUser:"",
+   findUser:function (userEmail) {
+    var arrayLength = usersData.length, i;
+    for (i = arrayLength - 1; i >= 0; i--) {
+     if (usersData[i].email === userEmail) {
+      this.currentUser = usersData[i];
+      break;
+     }
+    }
 
- };
+    return this;
+   },
+   formatName:function () {
+    if (this.currentUser) {
+     this.currentUser.fullName = titleCaseName (this.currentUser.firstName) + " " + titleCaseName (this.currentUser.lastName);
+    }
+    return this;
+
+   },
+
+   formatData:function () {
+    if (this.currentUser) {
+     this.currentUser.viewData = "Member name: " + this.currentUser.fullName +'\n'+"ID: " + this.currentUser.id +'\n'+ "Email: " + this.currentUser.email;
+    }
+    return this;
+   },
+
+   displayUser:function () {
+    if (!this.currentUser) return;
+
+    return(this.currentUser.viewData);
+
+   }
+  };
+
 
 // Driver code
-userController.findUser("awesomesquidward@yahoo.com").formatName().formatData().displayUser();
+
+console.log(userController.findUser("awesomesquidward@yahoo.com").formatName().formatData().displayUser());
 
 // result
 // Member name: Squidward Tentacles
